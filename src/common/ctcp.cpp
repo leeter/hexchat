7dd47dc405e4e87d975bfe1ee0e03ee2cb18be20
leeter <leeterfp@gmail.com>
2014-08-30 10:24:51 -0600

    fix issue caused by c++11 user defined literals

--- src/common/ctcp.cpp
+++ src/common/ctcp.cpp
@@ -143,10 +143,10 @@ ctcp_handle (session *sess, char *to, char *nick, char *ip,
 	if (!g_ascii_strcasecmp (msg, "VERSION") && !prefs.hex_irc_hide_version)
 	{
 #ifdef WIN32
-		snprintf (outbuf, sizeof (outbuf), "VERSION HexChat "PACKAGE_VERSION" [x%d] / %s",
+		snprintf (outbuf, sizeof (outbuf), "VERSION HexChat " PACKAGE_VERSION " [x%d] / %s",
 					 get_cpu_arch (), get_sys_str (true));
 #else
-		snprintf (outbuf, sizeof (outbuf), "VERSION HexChat "PACKAGE_VERSION" / %s",
+		snprintf (outbuf, sizeof (outbuf), "VERSION HexChat " PACKAGE_VERSION " / %s",
 					 get_sys_str (true));
 #endif
 		serv->p_nctcp (nick, outbuf);
