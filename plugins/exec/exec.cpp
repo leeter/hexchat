c62d5c9d59905e91cf6098d787afc34ee19a8b77
leeter <leeterfp@gmail.com>
2014-09-14 12:34:44 -0600

    c++ification and hopefully proper handling of unicode command lines

--- plugins/exec/exec.cpp
+++ plugins/exec/exec.cpp
@@ -22,140 +22,167 @@
 
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
-#include <time.h>
+#include <chrono>
+#include <codecvt>
+#include <cstring>
+#include <locale>
+#include <type_traits>
+#include <memory>
+#include <sstream>
+#include <string>
+#include <boost/algorithm/string.hpp>
+#if _MSC_VER >= 1900
+#define NOEXCEPT noexcept
+#else
+#define NOEXCEPT throw()
+#endif
 
 #include "hexchat-plugin.h"
 
-static hexchat_plugin *ph;   /* plugin handle */
-static char name[] = "Exec";
-static char desc[] = "Execute commands inside HexChat";
-static char version[] = "1.2";
+namespace {
+    static hexchat_plugin *ph;   /* plugin handle */
+    static char name[] = "Exec";
+    static char desc[] = "Execute commands inside HexChat";
+    static char version[] = "1.2";
 
-static int
-run_command(const char *const word[], const char *const word_eol[], void *userdata)
+    std::wstring widen(const std::string & to_widen)
+    {
+        std::wstring_convert<std::codecvt_utf8_utf16<wchar_t> > converter;
+        return converter.from_bytes(to_widen);
+    }
+
+    auto handle_deleter = [](HANDLE h) NOEXCEPT
+    {
+        if (h != INVALID_HANDLE_VALUE)
+            CloseHandle(h);
+    };
+
+    static int
+        run_command(const char *const word[], const char *const word_eol[], void *userdata) NOEXCEPT
+    {
+        if (std::strlen(word[2]) > 0)
+        {
+            char buffer[4096] = { 0 };
+            DWORD dwRead = 0;
+            DWORD dwLeft = 0;
+            DWORD dwAvail = 0;
+            bool announce = false;
+
+            std::unique_ptr<std::remove_pointer<HANDLE>::type, decltype(handle_deleter)> readPipe;
+            std::unique_ptr<std::remove_pointer<HANDLE>::type, decltype(handle_deleter)> writePipe;
+            STARTUPINFO sInfo = { 0 };
+            PROCESS_INFORMATION pInfo = { 0 };
+            SECURITY_ATTRIBUTES secattr = { 0 };
+
+            secattr.nLength = sizeof(secattr);
+            secattr.bInheritHandle = TRUE;
+            std::chrono::nanoseconds time_elapsed;
+            try
+            {
+                std::wostringstream commandLine(L"cmd.exe /c ", std::ios::ate);
+
+                if (boost::iequals("-O", word[2]))
+                {
+                    commandLine << widen(word_eol[3]);
+                    announce = true;
+                }
+                else
+                {
+                    commandLine << widen(word_eol[2]);
+                }
+
+                {
+                    HANDLE rdPipe;
+                    HANDLE wtPipe;
+                    CreatePipe(&rdPipe, &wtPipe, &secattr, 0); /* might be replaced with MyCreatePipeEx */
+                    readPipe.reset(rdPipe);
+                    writePipe.reset(wtPipe);
+                }
+
+                sInfo.cb = sizeof(STARTUPINFO);
+                sInfo.dwFlags = STARTF_USESTDHANDLES;
+                sInfo.hStdInput = NULL;
+                sInfo.hStdOutput = writePipe.get();
+                sInfo.hStdError = writePipe.get();
+
+                std::wstring process_cmdln(commandLine.str());
+                process_cmdln.push_back(L'\0');
+                CreateProcessW(0, &process_cmdln[0], 0, 0, TRUE, NORMAL_PRIORITY_CLASS | CREATE_NO_WINDOW, 0, 0, &sInfo, &pInfo);
+                writePipe.reset();
+                std::unique_ptr<std::remove_pointer<HANDLE>::type, decltype(handle_deleter)> hProcess(pInfo.hProcess);
+                std::unique_ptr<std::remove_pointer<HANDLE>::type, decltype(handle_deleter)> hThread(pInfo.hThread);
+
+                auto start = std::chrono::high_resolution_clock::now();
+                while (PeekNamedPipe(readPipe.get(), buffer, 1, &dwRead, &dwAvail, &dwLeft) && time_elapsed < std::chrono::seconds(10))
+                {
+                    if (dwRead)
+                    {
+                        if (ReadFile(readPipe.get(), buffer, sizeof(buffer) - 1, &dwRead, NULL) && dwRead != 0)
+                        {
+                            /* avoid garbage */
+                            buffer[dwRead] = '\0';
+
+                            if (announce)
+                            {
+                                std::istringstream inputbuf(buffer);
+                                /* Say each line seperately, TODO: improve... */
+                                for (std::string line; std::getline(inputbuf, line, '\n');)
+                                {
+                                    hexchat_commandf(ph, "SAY %s", line.c_str());
+                                }
+                            }
+                            else
+                                hexchat_printf(ph, "%s", buffer);
+                        }
+                    }
+                    else
+                    {
+                        /* this way we'll more likely get full lines */
+                        SleepEx(100, TRUE);
+                    }
+                    time_elapsed = std::chrono::high_resolution_clock::now() - start;
+                }
+            }
+            catch (const std::exception & ex)
+            {
+                hexchat_printf(ph, "An error occured during execution: %s", ex.what());
+            }
+
+            /* display a newline to separate things */
+            if (!announce)
+                hexchat_print(ph, "\n");
+
+            if (time_elapsed >= std::chrono::seconds(10))
+            {
+                hexchat_print(ph, "Command took too much time to run, execution aborted.\n");
+            }
+        }
+        else
+        {
+            hexchat_command(ph, "help exec");
+        }
+
+        return HEXCHAT_EAT_HEXCHAT;
+    }
+}
+int
+hexchat_plugin_init(hexchat_plugin *plugin_handle, char **plugin_name, char **plugin_desc, char **plugin_version, char *arg) NOEXCEPT
 {
-	char commandLine[1024];
-	char buffer[4096];
-	DWORD dwRead = 0;
-	DWORD dwLeft = 0;
-	DWORD dwAvail = 0;
-	time_t start;
-	double timeElapsed;
+    ph = plugin_handle;
 
-	char *token;
-	char *context = NULL;
-	int announce = 0;
+    *plugin_name = name;
+    *plugin_desc = desc;
+    *plugin_version = version;
 
-	HANDLE readPipe;
-	HANDLE writePipe;
-	STARTUPINFO sInfo; 
-	PROCESS_INFORMATION pInfo; 
-	SECURITY_ATTRIBUTES secattr; 
+    hexchat_hook_command(ph, "EXEC", HEXCHAT_PRI_NORM, run_command, "Usage: /EXEC [-O] - execute commands inside HexChat", 0);
+    hexchat_printf(ph, "%s plugin loaded\n", name);
 
-	ZeroMemory (&secattr, sizeof (secattr));
-	secattr.nLength = sizeof (secattr);
-	secattr.bInheritHandle = TRUE;
-
-	timeElapsed = 0.0;
-
-	if (strlen (word[2]) > 0)
-	{
-		strcpy (commandLine, "cmd.exe /c ");
-
-		if (!stricmp("-O", word[2]))
-		{
-			strcat (commandLine, word_eol[3]);
-			announce = 1;
-		}
-		else
-		{
-			strcat (commandLine, word_eol[2]);
-		}
-
-		CreatePipe (&readPipe, &writePipe, &secattr, 0); /* might be replaced with MyCreatePipeEx */
-
-		ZeroMemory (&sInfo, sizeof (sInfo));
-		ZeroMemory (&pInfo, sizeof (pInfo));
-		sInfo.cb = sizeof (sInfo);
-		sInfo.dwFlags = STARTF_USESTDHANDLES;
-		sInfo.hStdInput = NULL;
-		sInfo.hStdOutput = writePipe;
-		sInfo.hStdError = writePipe;
-
-		CreateProcess (0, commandLine, 0, 0, TRUE, NORMAL_PRIORITY_CLASS | CREATE_NO_WINDOW, 0, 0, &sInfo, &pInfo);
-		CloseHandle (writePipe);
-
-		start = time (0);
-		while (PeekNamedPipe (readPipe, buffer, 1, &dwRead, &dwAvail, &dwLeft) && timeElapsed < 10)
-		{
-			if (dwRead)
-			{
-				if (ReadFile (readPipe, buffer, sizeof (buffer) - 1, &dwRead, NULL) && dwRead != 0 )
-				{
-					/* avoid garbage */
-					buffer[dwRead] = '\0';
-
-					if (announce)
-					{
-						/* Say each line seperately, TODO: improve... */
-						token = strtok_s (buffer, "\n", &context);
-						while (token != NULL)
-						{
-							hexchat_commandf (ph, "SAY %s", token);
-							token = strtok_s (NULL, "\n", &context);
-						}
-					}
-					else
-						hexchat_printf (ph, "%s", buffer);
-				}
-			}
-			else
-			{
-				/* this way we'll more likely get full lines */
-				SleepEx (100, TRUE);
-			}
-			timeElapsed = difftime (time (0), start);
-		}
-
-		/* display a newline to separate things */
-		if (!announce)
-			hexchat_printf (ph, "\n");
-
-		if (timeElapsed >= 10)
-		{
-			hexchat_printf (ph, "Command took too much time to run, execution aborted.\n");
-		}
-
-		CloseHandle (readPipe);
-		CloseHandle (pInfo.hProcess);
-		CloseHandle (pInfo.hThread);
-	}
-	else
-	{
-		hexchat_command (ph, "help exec");
-	}
-
-	return HEXCHAT_EAT_HEXCHAT;
+    return 1;       /* return 1 for success */
 }
 
 int
-hexchat_plugin_init (hexchat_plugin *plugin_handle, char **plugin_name, char **plugin_desc, char **plugin_version, char *arg)
+hexchat_plugin_deinit(void) NOEXCEPT
 {
-	ph = plugin_handle;
-
-	*plugin_name = name;
-	*plugin_desc = desc;
-	*plugin_version = version;
-
-	hexchat_hook_command (ph, "EXEC", HEXCHAT_PRI_NORM, run_command, "Usage: /EXEC [-O] - execute commands inside HexChat", 0);
-	hexchat_printf (ph, "%s plugin loaded\n", name);
-
-	return 1;       /* return 1 for success */
-}
-
-int
-hexchat_plugin_deinit (void)
-{
-	hexchat_printf (ph, "%s plugin unloaded\n", name);
-	return 1;
+    hexchat_printf(ph, "%s plugin unloaded\n", name);
+    return 1;
 }
