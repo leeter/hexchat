6124942762ae659e18a3c4afc512a90fa88f02fb
leeter <leeterfp@gmail.com>
2014-09-28 11:51:41 -0600

    first pass on a parser for inbound messages, this is experimental at best I'm not sure about the performance. Also reserve throttled_connection as a wrapper for an outbound connection

---
+++ src/libirc/message.hpp
@@ -0,0 +1,52 @@
+/* HexChat
+* Copyright (C) 2014 Leetsoftwerx.
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
+*/
+
+#ifndef LIBIRC_MESSAGE_HPP
+#define LIBIRC_MESSAGE_HPP
+
+#include <string>
+#include <boost/optional.hpp>
+
+namespace irc
+{
+	struct message
+	{
+		enum numeric_reply
+		{
+			RPL_WELCOME = 1,
+			RPL_YOURHOST,
+			RPL_CREATED,
+			RPL_MYINFO,
+			RPL_BOUNCE,
+
+			RPL_TRACELINK = 200,
+			RPL_TRACECONNECTING
+		};
+		boost::optional<numeric_reply> command_n;
+		boost::optional<std::string> command_s;
+		boost::optional<std::string> prefix;
+		std::string nick;
+		std::string user;
+		std::string host;
+		boost::optional<std::string> params;
+	};
+
+	message parse(std::string inbound);
+}
+
+#endif
