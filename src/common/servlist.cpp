f2f0ec897fddb8de62c197bd3b66f84b5df97c82
leeter <mgetz@leetsoftwerx.com>
2014-12-14 19:34:06 -0700

    build fix.. need to make this RAII

--- src/common/servlist.cpp
+++ src/common/servlist.cpp
@@ -1340,7 +1340,7 @@ servlist_save (void)
 #ifndef WIN32
 	int first = FALSE;
 
-	buf = g_build_filename (get_xdir (), "servlist.conf", NULL);
+	gchar * buf = g_build_filename (get_xdir (), "servlist.conf", NULL);
 	if (g_access (buf, F_OK) != 0)
 		first = TRUE;
 #endif
