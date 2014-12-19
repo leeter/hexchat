8874814ab242d7dda55763d8d79bd406fcf53e82
leeter <mgetz@leetsoftwerx.com>
2014-12-18 20:36:36 -0700

    add missing headers

--- src/common/cfgfiles.cpp
+++ src/common/cfgfiles.cpp
@@ -16,6 +16,8 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
  */
 
+#include <algorithm>
+#include <iterator>
 #include <fcntl.h>
 #include <memory>
 #include <sstream>
