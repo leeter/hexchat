a72ca6ec67711059a3c0868af4b804c10a9666ad
leeter <mgetz@leetsoftwerx.com>
2014-12-20 14:40:19 -0700

    whitespace

--- src/common/outbound.cpp
+++ src/common/outbound.cpp
@@ -4234,74 +4234,76 @@ check_special_chars (char *cmd, bool do_ascii) /* check for %X */
 
 	std::string buf(len + 1, '\0');
 	std::locale locale;
-		while (cmd[j])
+	while (cmd[j])
+	{
+		switch (cmd[j])
 		{
-			switch (cmd[j])
+		case '%':
+			occur++;
+			if (	do_ascii &&
+					j + 3 < len &&
+				(std::isdigit(cmd[j + 1], locale) && std::isdigit(cmd[j + 2], locale) &&
+				std::isdigit(cmd[j + 3], locale)))
 			{
-			case '%':
-				occur++;
-				if (	do_ascii &&
-						j + 3 < len &&
-					(std::isdigit(cmd[j + 1], locale) && std::isdigit(cmd[j + 2], locale) &&
-					std::isdigit(cmd[j + 3], locale)))
+				tbuf[0] = cmd[j + 1];
+				tbuf[1] = cmd[j + 2];
+				tbuf[2] = cmd[j + 3];
+				tbuf[3] = 0;
+				buf[i] = atoi (tbuf);
+				glib_string utf(g_locale_to_utf8(&buf[0] + i, 1, 0, &utf_len, 0));
+				if (utf)
 				{
-					tbuf[0] = cmd[j + 1];
-					tbuf[1] = cmd[j + 2];
-					tbuf[2] = cmd[j + 3];
-					tbuf[3] = 0;
-					buf[i] = atoi (tbuf);
-					glib_string utf(g_locale_to_utf8(&buf[0] + i, 1, 0, &utf_len, 0));
-					if (utf)
-					{
 					std::copy_n(utf.get(), utf_len, buf.begin() + i);
-						i += (utf_len - 1);
-					}
-					j += 3;
-				} else
-				{
-					switch (cmd[j + 1])
-					{
-					case 'R':
-						buf[i] = '\026';
-						break;
-					case 'U':
-						buf[i] = '\037';
-						break;
-					case 'B':
-						buf[i] = '\002';
-						break;
-					case 'I':
-						buf[i] = '\035';
-						break;
-					case 'C':
-						buf[i] = '\003';
-						break;
-					case 'O':
-						buf[i] = '\017';
-						break;
-					case 'H':	/* CL: invisible text code */
-						buf[i] = HIDDEN_CHAR;
-						break;
-					case '%':
-						buf[i] = '%';
-						break;
-					default:
-						buf[i] = '%';
-						j--;
-						break;
-					}
-					j++;
-					break;
-			default:
-					buf[i] = cmd[j];
+					i += (utf_len - 1);
 				}
+				j += 3;
+			} else
+			{
+				switch (cmd[j + 1])
+				{
+				case 'R':
+					buf[i] = '\026';
+					break;
+				case 'U':
+					buf[i] = '\037';
+					break;
+				case 'B':
+					buf[i] = '\002';
+					break;
+				case 'I':
+					buf[i] = '\035';
+					break;
+				case 'C':
+					buf[i] = '\003';
+					break;
+				case 'O':
+					buf[i] = '\017';
+					break;
+				case 'H':	/* CL: invisible text code */
+					buf[i] = HIDDEN_CHAR;
+					break;
+				case '%':
+					buf[i] = '%';
+					break;
+				default:
+					buf[i] = '%';
+					j--;
+					break;
+				}
+				j++;
+				break;
+		default:
+				buf[i] = cmd[j];
 			}
-			j++;
-			i++;
 		}
-		buf[i] = 0;
-		if (occur)
+		j++;
+		i++;
+	}
+	buf[i] = 0;
+	if (occur)
+	{
 		std::copy(buf.cbegin(), buf.cend(), cmd);
+	}
 }
 
 static void
