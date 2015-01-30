302fc8fb71a5ceacae2b0c705ff5d601dda6b88d
leeter <mgetz@leetsoftwerx.com>
2015-01-30 08:56:24 -0700

    verify server certs and use more secure defaults

--- src/libirc/tcp_connection.cpp
+++ src/libirc/tcp_connection.cpp
@@ -48,7 +48,10 @@ namespace{
 		{
 			ssl_ctx.set_options(
 				boost::asio::ssl::context::no_sslv2 |
-				boost::asio::ssl::context::no_sslv3 );
+				boost::asio::ssl::context::no_sslv3 |
+				boost::asio::ssl::context::no_compression | 
+				boost::asio::ssl::context::single_dh_use |
+				SSL_OP_CIPHER_SERVER_PREFERENCE);
 			ssl_ctx.set_verify_mode(mode);
 		}
 		boost::asio::ssl::context ssl_ctx;
