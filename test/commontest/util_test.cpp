6930435af86b2fc57a333f369749d64980611587
leeter <mgetz@leetsoftwerx.com>
2015-02-21 19:33:44 -0700

    more tests

--- test/commontest/util_test.cpp
+++ test/commontest/util_test.cpp
@@ -4,6 +4,7 @@
 #define BOOST_TEST_DYN_LINK
 #endif
 #define BOOST_TEST_MODULE util_tests
+#include <algorithm>
 #include <cstring>
 #include <util.hpp>
 #include <boost/test/unit_test.hpp>
@@ -16,9 +17,25 @@ BOOST_AUTO_TEST_CASE(safe_strcpy_input_too_long)
 	const char* input = "veryveryvery";
 	// overallocate so we can check for buffer overflow
 	char out[15];
+	std::fill(std::begin(out), std::end(out), 'd');
 	safe_strcpy(out, input, 10);
 
 	BOOST_REQUIRE_EQUAL(std::strlen(out), 9);
+	BOOST_REQUIRE_EQUAL(out[9], 0);
+	BOOST_REQUIRE_EQUAL(out[10], 'd');
+}
+
+BOOST_AUTO_TEST_CASE(safe_strcpy_inputshorter_than_buffer)
+{
+	const char* input = "very";
+	// overallocate so we can check for buffer overflow
+	char out[15];
+	std::fill(std::begin(out), std::end(out), 'd');
+	safe_strcpy(out, input, 10);
+
+	BOOST_REQUIRE_EQUAL(std::strlen(out), 4);
+	BOOST_REQUIRE_EQUAL(out[4], 0);
+	BOOST_REQUIRE_EQUAL(out[5], 'd');
 }
 
 
