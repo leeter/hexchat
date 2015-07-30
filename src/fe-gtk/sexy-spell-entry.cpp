6a6dc15d5043fccefd852bd9dfbba2f6ccb14195
leeter <mgetz@leetsoftwerx.com>
2015-07-30 16:59:23 -0600

    final attempt?

--- src/fe-gtk/sexy-spell-entry.cpp
+++ src/fe-gtk/sexy-spell-entry.cpp
@@ -147,7 +147,10 @@ static int codetable_ref = 0;
 
 #ifndef G_ADD_PRIVATE
 
-#define G_ADD_PRIVATE(t) {}
+#define G_ADD_PRIVATE(TypeName) { \
+  TypeName##_private_offset = \
+    g_type_add_instance_private (g_define_type_id, sizeof (TypeName##Private)); \
+}
 
 static inline gpointer \
 sexy_spell_entry_get_instance_private(SexySpellEntry *self) \
